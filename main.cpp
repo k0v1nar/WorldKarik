a06cd123febcf541b86daafd8fc628bdba052ef7
k0v1nar <kostin.kirill.04@gmail.com>
2019-03-02 20:22:00 +0400

    ver11

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include<SFML/Window/VideoMode.hpp>
 using namespace gamebase;
 using namespace std;
-float w, h, w2, h2;
+float w, h, w3, h2;
 class MyApp : public App
 {
 	map<int,string > intToRes;
+	map<int, string> intToWeap;
 	map< string,int> resToInt;
+	map< string, int> weapToInt;
 	IntVec2 ch;
 	enum TypeIt
 	{
@@ -77,6 +79,7 @@ class MyApp : public App
 		bool active;
 		bool isLeft;
 		bool isRight;
+		int type;
 	};
 
 	struct Resource
@@ -98,6 +101,205 @@ class MyApp : public App
 		} data;
 		int x, y, num;
 	};
+	int seekItem(string i)
+	{
+		int b=0;
+		for (auto& a : slots)
+		{
+			if (!a.empty)
+			{
+				if (a.type == resources)
+				{
+					if (a.data.resource.type == resToInt[i])
+					{
+						b += a.data.resource.number;
+					}
+				}
+			}
+		}
+		return b;
+	}
+	void delItem(string i, int a)
+	{
+		for (auto& b : slots)
+		{
+			if (!b.empty)
+			{
+				if (b.type == resources)
+				{
+					if (b.data.resource.type == resToInt[i])
+					{
+						if (a > b.data.resource.number)
+						{
+							a -= b.data.resource.number;
+							b.empty = true;
+							updateSlot(b.num);
+						}
+						if (a <= b.data.resource.number)
+						{
+							b.data.resource.number-=a;
+							if (b.data.resource.number==0)
+							b.empty = true;
+							updateSlot(b.num);
+						}
+					}
+				}
+			}
+		}
+	}
+	int dropnumcr=1;
+	void menucraft(int i, string a)
+	{
+		if (i == 0)
+		{
+			Menudr.hide();
+			Max.show();
+			more.show();
+			dropnumcr = 1;
+		}
+		if (i == 1)
+		{
+			Menudr.hide();
+			for (auto& b : RDB[a].recipe)
+			{
+				delItem(b.first,dropnumcr*b.second);
+			}
+			seekSlot(a, DB[a].type, dropnumcr);
+			dropnumcr = 1;
+			Max.show();
+			more.show();
+		}
+		if (i == 2)
+		{
+			dropnumcr -= 1;
+			Max.show();
+			more.show();
+			if (dropnumcr == 1)
+			{
+				less.hide();
+				Min.hide();
+			}
+		}
+		if (i == 3)
+		{
+			dropnumcr += 1;
+			Min.show();
+			less.show();
+			if (dropnumcr == recipeCol)
+			{
+				Max.hide();
+				more.hide();
+			}
+		}
+		if (i == 4)
+		{
+			dropnumcr = 1;
+			Min.hide();
+			less.hide();
+			Max.show();
+			more.show();
+		}
+		if (i == 5)
+		{
+			dropnumcr = recipeCol;
+			Min.show();
+			less.show();
+			Max.hide();
+			more.hide();
+		}
+		coldrop << dropnumcr;
+		if (recipeCol == 1)
+		{
+			Max.hide();
+			more.hide();
+		}
+	}
+	int recipeCol = 0;
+	void craftItem(string i)
+	{
+		int min = 99999999;
+		for (auto& a : RDB[i].recipe)
+		{
+			int c = seekItem(a.first) / a.second;
+			if (c < min)
+			{
+				min = c;
+			}
+		}
+		recipeCol = min;
+		coldrop << 1;
+		Menudr.show();
+		less.hide();
+		Min.hide();
+		Max.show();
+		more.show();
+		if (recipeCol == 1)
+		{
+			less.hide();
+			more.hide();
+			Min.hide();
+			Max.hide();
+		}
+		if (recipeCol == 0)
+		{
+			Menudr.hide();
+		}
+		connect(back, menucraft, 0,i);
+		connect(cont, menucraft, 1,i);
+		connect(less, menucraft, 2,i);
+		connect(more, menucraft, 3,i);
+		connect(Min, menucraft, 4,i);
+		connect(Max, menucraft, 5,i);
+	}
+	void recipeLoad()
+	{
+		for (auto b = 1; b <= 5; b++)
+		{
+			auto cr = design.child<Layout>("cr" + toString(b - 1));
+			cr.child<Layout>("stolb").clear();
+		}
+		for (auto& b : RDB)
+		{
+			auto cr = design.child<Layout>("cr" + toString(b.second.from - 1));
+			auto b2 = cr.child<Layout>("stolb").load("recipe.json");
+			b2.child<Label>("name").setText(DB[b.first].name);
+			b2.child<Texture>("crpict").setImageName(DB[b.first].file);
+			for (auto& i : b.second.recipe)
+			{
+				auto bb = b2.child<Layout>("item").load("sl.json");
+				bb.child<Texture>("pict").setImageName(DB[i.first].file);
+				bb.child<Label>("col") << " x " << i.second;
+			}
+			connect(b2.child<Button>("create"),craftItem,b.first);
+		}
+		design.update();
+	}
+	void readRDB()
+	{
+		for (auto b = 1; b <= 5; b++)
+		{
+			ifstream input("data/recipe/recipe"+toString(b)+".txt");
+			int a = 0;
+				input >> a;
+			for (auto i = 0; i < a; i++)
+			{
+				string name;
+				input >> name;
+				int c;
+				input >> c;
+				RDB[name].col = c;
+				RDB[name].from = b;
+				for (auto i2 = 0; i2 < c; i2++)
+				{
+					string namerec;
+					input >> namerec;
+					int c2;
+					input >> c2;
+					RDB[name].recipe[namerec] = c2;
+				}
+			}
+		}
+	}
 	void readDBR()
 	{
 		ifstream input("data/obj/db.txt");
@@ -187,6 +389,8 @@ class MyApp : public App
 			}
 			else if (b=="weapon")
 			{
+				intToWeap[res] = name2;
+				weapToInt[name2] = res;
 				string name;
 				string file;
 				string typ;
@@ -223,6 +427,7 @@ class MyApp : public App
     {
 		readDB();
 		readDBR();
+		readRDB();
 		Menu.hide();
 		int i = 0;
 		for (int y = 0; y < 5; y++)
@@ -231,7 +436,7 @@ class MyApp : public App
 				Slot slott;
 				slott.num = i;
 				slots.push_back(slott);
-				auto& slo = slot.load("slot.json", x*w2, y*h2);
+				auto& slo = slot.load("slot.json", x*w3, y*h2);
 				slo.child<Texture>("obj").hide();
 				slo.child<DrawObj>("num").hide();
 				i++;
@@ -267,7 +472,47 @@ class MyApp : public App
 		connect(more, drm, 3);
 		connect(Min, drm, 4);
 		connect(Max, drm, 5);
+		connect(use, drawQieup);
     }
+	void drawQieup()
+	{
+		if (slots[nowSlot].type == weapons)
+		{
+			MenuW.show();
+		}
+		if (slots[nowSlot].type == potions)
+		{
+			MenuP.show();
+		}
+		if (slots[nowSlot].type == armors)
+		{
+			if (slots[nowSlot].data.armor.type == slots[nowSlot].data.armor.boots)
+			{
+
+			}
+			if (slots[nowSlot].data.armor.type == slots[nowSlot].data.armor.chesplat)
+			{
+
+			}
+			if (slots[nowSlot].data.armor.type == slots[nowSlot].data.armor.helmet)
+			{
+
+			}
+			if (slots[nowSlot].data.armor.type == slots[nowSlot].data.armor.leggs)
+			{
+
+			}
+		}
+	}
+	void conmenu()
+	{
+			connect(back, drm, 0);
+			connect(cont, drm, 1);
+			connect(less, drm, 2);
+			connect(more, drm, 3);
+			connect(Min, drm, 4);
+			connect(Max, drm, 5);
+	}
 	int dropnum = 1;
 	void drm(int i)
 	{
@@ -331,6 +576,11 @@ class MyApp : public App
 			more.hide();
 		}
 		coldrop << dropnum;
+		if (slots[nowSlot].data.resource.number == 1)
+		{
+			Max.hide();
+			more.hide();
+		}
 	}
 	void nextCr(int i)
 	{
@@ -350,8 +600,14 @@ class MyApp : public App
 		b.child<DrawObj>("num").show();
 		if (a.type == resources)
 		{
+			b.child<DrawObj>("num").show();
 			b.child<Label>("col").setText(toString(a.data.resource.number));
 			b.child<Texture>("obj").setImageName(DB[intToRes[a.data.resource.type]].file);
+		}
+		if (a.type == weapons)
+		{
+			b.child<DrawObj>("num").hide();
+			b.child<Texture>("obj").setImageName(DB[intToWeap[a.data.weapon.type]].file);
 		}
 		b.child<Texture>("obj").show();
 		design.update();
@@ -367,39 +623,12 @@ class MyApp : public App
 				updateSlot(a.num);
 			}
 			design.update();
-			fieldInventor.setView(4 * w2, 2 * h2);
+			conmenu();
+			fieldInventor.setView(4 * w3, 2 * h2);
 		}
 		if (i == 4)
 		{
-			for (int a = 1; a <= 5; a++)
-			{
-				auto cr = design.child<Layout>("cr" + toString(a - 1));
-				cr.child<Layout>("stolb").clear();
-				ifstream input("data/recipe/recipe" + toString(a) + ".txt");
-				int yyy = 0;
-				input >> yyy;
-				cout << yyy << endl;
-				for (int y = 0; y < yyy; y++)
-				{
-					auto b = cr.child<Layout>("stolb").load("recipe.json");
-					string i;
-					input >> i;
-					b.child<Label>("name").setText(DB[i].name);
-					b.child<Texture>("crpict").setImageName(DB[i].file);
-					int a;
-					input >> a;
-					for (int x = 0; x < a; x++)
-					{
-						auto bb = b.child<Layout>("item").load("sl.json");
-						input >> i;
-						bb.child<Texture>("pict").setImageName(DB[i].file);
-						input >> i;
-						bb.child<Label>("col") << " x " << i;
-					}
-				}
-				input.close();
-				design.update();
-			}
+			recipeLoad();
 		}
 	}
 	void updateNO(vector<GameObj> objs)
@@ -413,9 +642,8 @@ class MyApp : public App
 			objs.front().anim.play("highlight");
 		}
 	}
-	void seekSlot(string type)
+	void seekSlot(string type, TypeIt i, int c)
 	{
-		int b=1;
 		for (auto& a : slots)
 		{
 			if (a.empty)
@@ -424,23 +652,49 @@ class MyApp : public App
 			{
 				if (!a.data.resource.full() && intToRes[a.data.resource.type]== type)
 				{
-					a.data.resource.number++;
-					b--;
-					break;
+					if (a.data.resource.number + c <= 20)
+					{
+						a.data.resource.number += c;
+						c = 0;
+						break;
+					}
+					c -= 20 - a.data.resource.number;
+					a.data.resource.number = 20;
 				}
 			}
+			else
+			if (a.type == potions)
+			{
+
+			};
 		}
-		if (b > 0)
+		if (c > 0)
 		{
 			for (auto& a : slots)
 			{
 				if (a.empty)
 				{
-					a.type = resources;
-					a.data.resource.type = resToInt[type];
-					a.data.resource.number=1;
 					a.empty = false;
-					break;
+					if (i == resources)
+					{
+						a.type = resources;
+						a.data.resource.type = resToInt[type];
+						if (c <= 20)
+						{
+							a.data.resource.number = c;
+							break;
+						}
+						c -= 20;
+						a.data.resource.number = 20;
+					}
+					if (i == weapons)
+					{
+						a.type = weapons;
+						a.data.weapon.type = weapToInt[type];
+						break;
+					}
+					
+					
 				}
 			}
 		}
@@ -450,7 +704,7 @@ class MyApp : public App
 	int Vec2ToInt(Vec2 i)
 	{
 		IntVec2 a;
-		a.x = (i.x + w2 / 2) / w2;
+		a.x = (i.x + w3 / 2) / w3;
 		a.y = (i.y + h2 / 2) / h2;
 		int b = a.y * 9 + a.x;
 		return b;
@@ -475,8 +729,12 @@ class MyApp : public App
 		Menu.hide();
 		Min.hide();
 		less.hide();
-		Max.show();
-		more.show();
+		if (slots[nowSlot].data.resource.number> 1)
+		{
+			Max.show();
+			more.show();
+		}
+		
 	}
 	int isMouse=0;
     void process(Input input)
@@ -496,7 +754,7 @@ class MyApp : public App
 				hideCursor();
 				return;
 			}
-			if (changer.selected()==0 && !Menudr.isVisible())
+			if (changer.selected()==0 && !Menudr.isVisible() && !MenuW.isVisible() && !MenuP.isVisible())
 			{ 
 				if (input.justPressed(MouseRight))
 				{
@@ -513,7 +771,7 @@ class MyApp : public App
 						if (!slots[nowSlot].empty)
 						{
 							auto b = slot.get(nowSlot);
-							Menu.setPos(b.pos().x + w2, b.pos().y);
+							Menu.setPos(b.pos().x + w3, b.pos().y);
 							Menu.show();
 							Menu.child<Button>("use").show();
 							if (slots[nowSlot].type == resources)
@@ -524,19 +782,11 @@ class MyApp : public App
 						design.update();
 					}
 				}
+			
 			if (!Menu.isVisible())
 				if (input.justPressed(MouseLeft))
 				{
-					if (isMouse == 2
-						&& !impl::isMouseOn(
-							dynamic_cast<impl::Drawable*>(Menu.getImpl()->getInternalObj().get())))
-					{
-						Menu.hide();
-						nowSlot = -1;
-						isMouse = 0;
-						design.update();
-					}
-					else if (isMouse == 0)
+					 if (isMouse == 0)
 					{
 						isMouse = 1;
 						nowSlot = Vec2ToInt(fieldInventor.mousePos());
@@ -556,6 +806,19 @@ class MyApp : public App
 					}
 				}
 			}
+			if (Menu.isVisible())
+				if (input.justPressed(MouseLeft))
+				{
+					if (isMouse == 2
+						&& !impl::isMouseOn(
+							dynamic_cast<impl::Drawable*>(Menu.getImpl()->getInternalObj().get())))
+					{
+						Menu.hide();
+						nowSlot = -1;
+						isMouse = 0;
+						design.update();
+					}
+				}
 		}
 		if (selector.selected() == 1)
 		{
@@ -582,34 +845,7 @@ class MyApp : public App
 				selector.select(2);
 				nexti(4);
 
-				for (int a = 1; a <= 5; a++)
-				{
-					auto cr = design.child<Layout>("cr" + toString(a - 1));
-					cr.child<Layout>("stolb").clear();
-					ifstream input("data/recipe/recipe" + toString(a) + ".txt");
-					int yyy = 0;
-					input >> yyy;
-					cout << yyy << endl;
-					for (int y = 0; y < yyy; y++)
-					{
-						auto b = cr.child<Layout>("stolb").load("recipe.json");
-						string i;
-						input >> i;
-						b.child<Label>("name").setText(DB[i].name);
-						b.child<Texture>("crpict").setImageName(DB[i].file);
-						int a;
-						input >> a;
-						for (int x = 0; x < a; x++)
-						{
-							auto bb = b.child<Layout>("item").load("sl.json");
-							input >> i;
-							bb.child<Texture>("pict").setImageName(DB[i].file);
-							input >> i;
-							bb.child<Label>("col") << " x " << i;
-						}
-					}
-					input.close();
-				}
+				recipeLoad();
 				design.update();
 				showCursor();
 			}
@@ -676,6 +912,7 @@ class MyApp : public App
 			{
 				selector.select(2);
 				nexti(0);
+				conmenu();
 				showCursor();
 			}
 			if (input.justPressed(E))
@@ -700,7 +937,7 @@ class MyApp : public App
 								int dd2 = randomInt(1, 100);
 								if (dd2 <= b2.chance)
 								{
-									seekSlot(b2.name);
+									seekSlot(b2.name, resources,1);
 								}
 							}
 							break;
@@ -827,7 +1064,13 @@ class MyApp : public App
 		TypE type;
 		object obect;
 	};
-		
+	struct Recipe
+	{
+		int col;
+		int from;
+		map<string, int> recipe;
+	};
+	map<string, Recipe> RDB;
 	map<string, Item> DB;
 	map<string, map<string, RObj>>DBR;
 	vector <Slot> slots;
@@ -940,6 +1183,11 @@ class MyApp : public App
 	FromDesign(Button, less);
 	FromDesign(Button, Min);
 	FromDesign(Button, Max);
+	FromDesign(Button, use);
+	FromDesign(Button, w1);
+	FromDesign(Button, w2);
+	FromDesign(Button, p1);
+	FromDesign(Button, p2);
 	FromDesign(GameView, field);
 	FromDesign(GameObj, player);
 	FromDesign(Selector, selector);
@@ -957,6 +1205,8 @@ class MyApp : public App
 	FromDesign(Layout, cr4);
 	FromDesign(Layout, Menu);
 	FromDesign(Layout, Menudr);
+	FromDesign(Layout, MenuW);
+	FromDesign(Layout, MenuP);
 	FromDesign(Label, coldrop);
 	GameObj nowObj;
 	IntVec2 p;
@@ -971,7 +1221,7 @@ int main(int argc, char** argv)
 	randomize();
 	w = sf::VideoMode::getDesktopMode().width / 10.0;
 	h = sf::VideoMode::getDesktopMode().height / 6.0;
-	w2 = sf::VideoMode::getDesktopMode().width * 0.11;
+	w3 = sf::VideoMode::getDesktopMode().width * 0.11;
 	h2 = sf::VideoMode::getDesktopMode().height * 0.19;
 	app.setSize(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height);
 	app.setMinSize(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height);
